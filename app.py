991505e1ab16571fadfb199de90f0dff5e8745ed
aruzpanel <haqberdiyevaruz9@gmail.com>
2025-10-30 11:45:54 +0500

    Update app.py

--- app.py
+++ app.py
@@ -235,7 +235,7 @@ async def send_request(encrypted_uid, token, url):
             'Expect': "100-continue",
             'X-Unity-Version': "2018.4.11f1",
             'X-GA': "v1 1",
-            'ReleaseVersion': "OB51"
+            'ReleaseVersion': "OB50"
         }
         async with aiohttp.ClientSession() as session:
             async with session.post(url, data=edata, headers=headers) as response:
@@ -314,7 +314,7 @@ def make_request(encrypt, region, token):
             'Expect': "100-continue",
             'X-Unity-Version': "2018.4.11f1",
             'X-GA': "v1 1",
-            'ReleaseVersion': "OB51"
+            'ReleaseVersion': "OB50"
         }
         response = requests.post(url, data=edata, headers=headers, verify=False)
         
@@ -617,4 +617,5 @@ def public_aruzlike():
 
 if __name__ == '__main__':
     app.run(threaded=True, use_reloader=False)
+
 
